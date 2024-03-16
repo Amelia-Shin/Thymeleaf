0c69078d39cf19cd04c6d5fe1522ad051c2fb458
DESKTOP-FHGO7O0\gmldn <gmldnjs0402@naver.com>
2024-03-16 15:03:58 +0900

    thymeleaf - URL 링크 사용법 (path, query)

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -73,8 +73,16 @@ public class BasicController {
     @GetMapping("/date")
     public String date(Model model) {
         model.addAttribute("localDateTime", LocalDateTime.now());
-        return "basic/date";
 
+        return "basic/date";
+    }
+
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
     }
 
     @Data
